acf0c478ea5ac174970e9b93ce8340f4e11e8673
Jeff Lucas <jslucas1@msn.com>
2019-08-06 14:48:32 -0500

    updated connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //const connectionURL = 'mongodb://127.0.0.1:27017/swami';  //that's the localhost IP and port
-const connectionURL = 'mongodb+srv://swamiapp:<Rolltide1!>@cluster0-s02ok.mongodb.net/test'; //that's the mongo url
+const connectionURL = 'mongodb+srv://swamiapp:<Rolltide1!>@cluster0-s02ok.mongodb.net/test?retryWrites=true&w=majority'; //that's the mongo url
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
