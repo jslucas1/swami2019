bc47c6431a2501af0050956e850c6fe09b18f9af
Jeff Lucas <jslucas1@msn.com>
2019-08-06 13:27:04 -0500

    added mongo url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionURL = 'mongodb://127.0.0.1:27017/swami';  //that's the localhost IP and port
+//const connectionURL = 'mongodb://127.0.0.1:27017/swami';  //that's the localhost IP and port
+const connectionURL = 'mongodb://<dbuser>:<dbpassword>@ds259787.mlab.com:59787/heroku_7przdzwp'; //that's the heroku url
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
